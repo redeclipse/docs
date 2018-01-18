c1aa291adee8609e778def4f326cfdcfe1db566d
Quinton Reeves <qreeves@gmail.com>
2018-01-18 13:33:21 +1000

    fix docs redir

--- src/semaphore.sh
+++ src/semaphore.sh
@@ -36,7 +36,8 @@ semabuild_build() {
         SEMABUILD_REDIRECT="0"
         if [ "${m}" = "Home" ]; then
             echo "redirect_from:" >> "${1}"
-            echo "  - /${2}" >> "${1}"
+            echo "  - /${2}/" >> "${1}"
+            echo "  - /${2}/Main_Page" >> "${1}"
             SEMABUILD_REDIRECT="1"
         elif [ "${m}" != "${q}" ]; then
             echo "redirect_from:" >> "${1}"
