8d6b40e108eb53d2f3c8295abc650e972dc256c2
Quinton Reeves <qreeves@gmail.com>
2019-10-15 16:47:19 +1000

    change docs redirection

--- src/semaphore.sh
+++ src/semaphore.sh
@@ -42,7 +42,7 @@ semabuild_build() {
             SEMABUILD_REDIRECT="1"
         elif [ "${m}" != "${q}" ]; then
             echo "redirect_from:" >> "${1}"
-            echo "  - /${2}/${m}/" >> "${1}"
+            echo "  - /${2}/${m}" >> "${1}"
             SEMABUILD_REDIRECT="1"
         fi
         if [ "${q}" != "${r}" ]; then
@@ -50,8 +50,8 @@ semabuild_build() {
                 echo "redirect_from:" >> "${1}"
                 SEMABUILD_REDIRECT="1"
             fi
-            echo "  - /${2}/${r}/" >> "${1}"
-            echo "  - /wiki/${r}/" >> "${1}"
+            echo "  - /${2}/${r}" >> "${1}"
+            echo "  - /wiki/${r}" >> "${1}"
         fi
         echo "---" >> "${1}"
         echo "* TOC" >> "${1}"
