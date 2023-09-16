5ea0eb128d8d155f0edf46d53dd9d0afdab27352
Quinton Reeves <qreeves@gmail.com>
2023-09-16 11:55:19 +1000

    implement semaphore 2.0 build

--- .semaphore/build.sh
+++ .semaphore/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-SEMABUILD_PWD=`pwd`
+SEMABUILD_GIT="${HOME}/${SEMAPHORE_GIT_DIR}"
 SEMABUILD_DEST="https://${GITHUB_TOKEN}:x-oauth-basic@github.com/redeclipse/redeclipse.github.io.git"
 SEMABUILD_DESTNAME="www"
 SEMABUILD_DESTPWD="${HOME}/${SEMABUILD_DESTNAME}"
@@ -8,13 +8,17 @@ SEMABUILD_DESTDOCS="${SEMABUILD_DESTPWD}/docs"
 SEMABUILD_DEPLOY="false"
 
 semabuild_setup() {
+    echo "setting up ${SEMAPHORE_GIT_BRANCH}.."
+
     git config --global user.email "noreply@redeclipse.net" || return 1
     git config --global user.name "Red Eclipse" || return 1
     git config --global credential.helper store || return 1
     echo "https://${GITHUB_TOKEN}:x-oauth-basic@github.com" > "${HOME}/.git-credentials"
+
     pushd "${HOME}" || return 1
     git clone --depth 1 "${SEMABUILD_DEST}" "${SEMABUILD_DESTNAME}" || return 1
     popd || return 1
+
     return 0
 }
 
@@ -22,6 +26,7 @@ semabuild_build() {
     m=`echo "${2}" | sed -e "s/^\(.*\)\.\([^.]*\)$/\1/"`
     n=`echo "${2}" | sed -e "s/^\(.*\)\.\([^.]*\)$/\2/"`
     o=`echo "${m}" | sed -e "s/^\(.\).*$/\1/"`
+
     if [ "${o}" != "_" ] && [ "${n}" = "md" ]; then
         p=`echo "${m}" | sed -e "s/[-_]/ /g;s/  / /g;s/^ //g;s/ $//g"`
         q=`echo "${m}" | sed -e "s/[_]/-/g;s/--/-/g;s/^-//g;s/-$//g"`
@@ -29,6 +34,7 @@ semabuild_build() {
         s=`echo "${p}" | sed -e "s/\//: /g"`
         t="$(echo "${s:0:1}" | tr '[:lower:]' '[:upper:]')${s:1}"
         u=`echo "${m}" | cut -d"/" -f2-`
+
         echo "CONVERT: ${m} (${n}) - ${p} (${q}) > ${1}"
         echo "---" > "${1}"
         echo "title: ${t}" >> "${1}"
@@ -36,6 +42,7 @@ semabuild_build() {
         echo "origfile: ${2}" >> "${1}"
         echo "origtitle: ${u}" >> "${1}"
         echo "permalink: /docs/${q}" >> "${1}"
+
         SEMABUILD_REDIRECT="0"
         echo "redirect_from:" >> "${1}"
         if [ "${m}" = "Home" ]; then
@@ -51,8 +58,10 @@ semabuild_build() {
         fi
         echo "  - /wiki/${r}/" >> "${1}"
         echo "---" >> "${1}"
+
         echo "* TOC" >> "${1}"
         echo "{:toc}" >> "${1}"
+
         c=`cat "${3}"`
         f=`echo "${c}" | sed -e "s/)/)\n/g" | grep "\[.*\]\([^:]*.md\)" | sed -e "s/.*\[\([^]]*\)\](\([^):]*\))$/\2/;s/^\([^#]*\)#.*$/\1/" | sort | uniq`
         for d in ${f}; do
@@ -65,12 +74,13 @@ semabuild_build() {
                 echo "found."
             fi
         done
+
         echo "${c}" | sed -e "s/\](\([^)]*\)\.md)/](\1)/g;s/\](\([^#)]*\)\.md#\([^)]*\))/](\1#\L\2)/g;s/http:\/\/redeclipse\.net//g;s/http:\/\/www.redeclipse\.net//g" >> "${1}"
     fi
 }
 
 semabuild_process() {
-    pushd "${SEMABUILD_PWD}" || return 1
+    pushd "${SEMABUILD_GIT}" || return 1
     for i in *; do
         if [ -d "${i}" ]; then
             if [ "${i}" = "images" ]; then
@@ -90,19 +100,20 @@ semabuild_process() {
         fi
     done
     popd || return 1
+
     pushd "${SEMABUILD_DESTDOCS}" || return 1
     for i in *; do
         if [ -d "${i}" ]; then
             if [ "${i}" != "src" ] && [ "${i}" != "bits" ]; then
                 pushd "${i}" || return 1
                 for j in *; do
-                    if [ ! -d "${j}" ] &&  [ ! -e "${SEMABUILD_PWD}/${i}/${j}" ]; then
+                    if [ ! -d "${j}" ] &&  [ ! -e "${SEMABUILD_GIT}/${i}/${j}" ]; then
                         git rm "${j}" || return 1
                     fi
                 done
                 popd || return 1
             fi
-        elif [ ! -e "${SEMABUILD_PWD}/${i}" ]; then
+        elif [ ! -e "${SEMABUILD_GIT}/${i}" ]; then
             git rm "${i}" || return 1
         fi
     done
@@ -111,14 +122,20 @@ semabuild_process() {
 
 semabuild_update() {
     pushd "${SEMABUILD_DESTPWD}" || return 1
+
     git add * || return 1
     for i in *; do
         if [ -d "${i}" ] && [ "${i}" != ".git" ] && [ "${i}" != "bits" ]; then
             git add "${i}"/* || return 1
         fi
     done
-    git commit -a -m "Build docs:${SEMAPHORE_BUILD_NUMBER} from ${REVISION}"  && git pull --rebase && git push -u origin master
+
+    git commit -a -m "Build docs:${SEMAPHORE_WORKFLOW_NUMBER} from ${SEMAPHORE_GIT_SHA}" || return 1
+    git pull --rebase || return 1
+    git push -u origin master || return 1
+
     popd || return 1
+
     return 0
 }
 
